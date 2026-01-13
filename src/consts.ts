55d6a165fa960d837d19429b74ec3f03c329f5cf
thexeromin <thexeromin@gmail.com>
2026-01-13 14:26:31 +0530

    add: assets, base meta tags, global body style

---
+++ src/consts.ts
@@ -0,0 +1,6 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = "Abhijit | Full stack Engineer";
+export const SITE_DESCRIPTION =
+  "Portfolio and blog of Abhijit, a full stack software engineer building scalable web applications and sharing engineering insights.";
